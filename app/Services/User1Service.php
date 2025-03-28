0fa8c700a132c91ebe32b1481e7ec0f74eb1c5ab
Kryllee <k.laderaaa@gmail.com>
2025-03-29 00:54:36 +0800

    amdying

---
+++ app/Services/User1Service.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+use App\Traits\ConsumesExternalService;
+
+class User1Service{
+
+    use ConsumesExternalService;
+    
+    /**
+     * The base uri to consume the User1 Service
+     * @var string
+     */
+    public $baseUri;
+
+    public function __construct()
+    {
+        $this->baseUri = config('services.users1.base_uri');
+    }
+    
+    /**
+     * Obtain the full list of Users from User1 Site
+     * @return string
+     */
+
+     public function obtainUsers1()
+     {
+         // This code will call the GET localhost:8000/users (our site1)
+         return $this->performRequest('GET', '/users');
+     }
+}     
