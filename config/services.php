3853d9b75b0203064f1aa4f687ccdccd7c51832b
Kryllee <k.laderaaa@gmail.com>
2025-04-15 01:39:28 +0800

    Update

--- config/services.php
+++ config/services.php
@@ -3,10 +3,10 @@
 return [
     'users1' => [
         'base_uri' => env('USER1_SERVICE_BASE_URL', 'https://microservice-ddsbe1-e8na.onrender.com'),
-        'secret' => env('9UaX51OcKg6aCC7W36qRXbIj9Rjpp6au'),
+        'secret' => env('USERS1_SERVICE_SECRET','9UaX51OcKg6aCC7W36qRXbIj9Rjpp6au'),
     ],
     'users2' => [
         'base_uri' => env('USER2_SERVICE_BASE_URL', 'https://microservice-ddsbe2-zggv.onrender.com'),
-        'secret' => env('iAZPyurjN0LIYZ0BhqaRrZ5vBIDiVvgf'),
+        'secret' => env('USERS2_SERVICE_SECRET','iAZPyurjN0LIYZ0BhqaRrZ5vBIDiVvgf'),
     ],
 ];
